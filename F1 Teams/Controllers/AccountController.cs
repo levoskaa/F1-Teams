041ce96f5ab55f83eaeb2110efacff3b469e4a6b
levoskaa <laurinyecz.levi@gmail.com>
2020-06-10 15:05:38 +0200

    Create Account Controller and View

---
+++ F1 Teams/Controllers/AccountController.cs
@@ -0,0 +1,68 @@
+﻿using F1Teams.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace F1Teams.Controllers
+{
+    /// <summary>
+    /// Controller for managing user authentication.
+    /// </summary>
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// Provides the APIs for user sign in.
+        /// </summary>
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        /// <summary>
+        /// Data received in a sign in request.
+        /// </summary>
+        [BindProperty]
+        public SignInViewModel signInData { get; set; }
+
+        public AccountController(SignInManager<IdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Processes the sign in requests.
+        /// </summary>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SignInPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(signInData.UserName, signInData.Password, false, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid username & password combination.");
+                }
+            }
+            return View("SignIn", signInData);
+        }
+
+        /// <summary>
+        /// Processes the sign out requests.
+        /// </summary>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SignOut()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ F1 Teams/ViewModels/SignInViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace F1Teams.ViewModels
+{
+    /// <summary>
+    /// ViewModel for the sign in page, contains the data necessary for the sign in operation.
+    /// </summary>
+    public class SignInViewModel
+    {
+        [Required(ErrorMessage = "Username is a required field!")]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is a required field!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
