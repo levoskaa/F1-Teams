011c9e65603aa77f881fabfc8b9347a77636d714
levoskaa <laurinyecz.levi@gmail.com>
2020-06-10 15:43:12 +0200

    Seed admin user into database

--- F1 Teams/DAL/EfDbContext/ApplicationDbContext.cs
+++ F1 Teams/DAL/EfDbContext/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace F1Teams.DAL.EfDbContext
 {
@@ -13,26 +14,45 @@ namespace F1Teams.DAL.EfDbContext
 
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(modelBuilder);
+            base.OnModelCreating(builder);
 
-            modelBuilder.Entity<DbTeam>()
+            builder.Entity<DbTeam>()
                 .ToTable("teams")
                 .HasKey(team => team.Id);
-            modelBuilder.Entity<DbTeam>()
+            builder.Entity<DbTeam>()
                 .Property(team => team.Name)
                 .HasMaxLength(25)
                 .IsRequired(required: true);
-            modelBuilder.Entity<DbTeam>()
+            builder.Entity<DbTeam>()
                 .Property(team => team.YearFormed);
-            modelBuilder.Entity<DbTeam>()
+            builder.Entity<DbTeam>()
                 .Property(team => team.ChampionshipsWon);
-            modelBuilder.Entity<DbTeam>()
+            builder.Entity<DbTeam>()
                 .Property(team => team.HasPaidEntryFee)
                 .IsRequired(required: true);
 
-            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole { Name = "admin", NormalizedName = "Admin".ToUpper() });
+            // Seeding data
+            string roleId = Guid.NewGuid().ToString();
+            string userId = Guid.NewGuid().ToString();
+            var passwordHasher = new PasswordHasher<IdentityUser>();
+            var user = new IdentityUser
+            {
+                Id = userId,
+                UserName = "admin",
+                NormalizedUserName = "ADMIN",
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+            user.PasswordHash = passwordHasher.HashPassword(user, "f1test2018");
+            // Seed admin role
+            builder.Entity<IdentityRole>()
+                .HasData(new IdentityRole { Id = roleId, Name = "admin", NormalizedName = "ADMIN" });
+            // Seed admin user
+            builder.Entity<IdentityUser>()
+                .HasData(user);
+            builder.Entity<IdentityUserRole<string>>()
+                .HasData(new IdentityUserRole<string> { RoleId = roleId, UserId = userId });
         }
     }
 }
